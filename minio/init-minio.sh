25bf24caa4998ddb78069a7db792911b911ac486
D3nisPi <d3nis.p1shchik@gmail.com>
2025-07-02 01:59:52 +0300

    Изменение конфига

--- minio/init-minio.sh
+++ minio/init-minio.sh
@@ -13,19 +13,4 @@ mc policy set public minio/$MINIO_BUCKET
 
 echo "Uploading test data..."
 
-TOTAL_FILES=$(find /test-data -type f | wc -l)
-echo "Found $TOTAL_FILES files to upload"
-
-mc cp -r /test-data/ minio/$MINIO_BUCKET/test-data/
-
-echo "Verifying uploaded data..."
-
-LOADED_FILES=$(mc ls -r minio/$MINIO_BUCKET/test-data/ | wc -l)
-
-if [ "$LOADED_FILES" -ne "$TOTAL_FILES" ]; then
-  echo "ERROR: uploaded only $LOADED_FILES/$TOTAL_FILES files"
-  exit 1
-fi
-
-echo "Successfully uploaded $LOADED_FILES/$TOTAL_FILES files"
-exit 0
+mc cp -r /test-data/ minio/$MINIO_BUCKET/
