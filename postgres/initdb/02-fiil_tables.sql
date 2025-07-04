354e15f4ee6a4ead01a7d91613e3975a5bb0094a
D3nisPi <125587370+D3nisPi@users.noreply.github.com>
2025-07-04 11:19:18 +0300

    Update 02-fiil_tables.sql

--- postgres/initdb/02-fiil_tables.sql
+++ postgres/initdb/02-fiil_tables.sql
@@ -21,6 +21,12 @@ INSERT INTO books (id, title, year_written) VALUES
 (3, 'Основание', '1951'),
 (4, 'Хоббит, или Туда и обратно', '1937');
 
+INSERT INTO users (id, name, salt, password_hash, refresh_token_uuid) VALUES
+(1, 'Alice', 'salt1', 'hash1', 'uuid1'),
+(2, 'Bob', 'salt2', 'hash2', 'uuid2'),
+(3, 'Charlie', 'salt3', 'hash3', 'uuid3'),
+(4, 'Diana', 'salt4', 'hash4', 'uuid4');
+
 INSERT INTO books_authors (book_id, author_id) VALUES 
 (1, 1),
 (2, 2),
@@ -38,3 +44,17 @@ INSERT INTO books_locations (book_id, extension_id, location) VALUES
 (2, 2, '/2.epub'),
 (3, 3, '/3.fb2'),
 (4, 2, '/4.epub');
+
+INSERT INTO favourite_books (user_id, book_id, added_ts) VALUES 
+(1, 1, '2025-07-02T10:23:54'),
+(1, 2, '2025-03-02T10:23:54'),
+(2, 2, '2025-06-02T10:23:54'),
+(3, 1, '2025-05-02T10:23:54'),
+(4, 4, '2025-01-02T10:23:54');
+
+INSERT INTO read_history (user_id, book_id, last_read_ts) VALUES 
+(1, 1, '2024-07-02T10:23:54'),
+(1, 2, '2025-03-10T10:23:54'),
+(2, 3, '2025-01-12T10:23:54'),
+(3, 3, '2024-04-02T10:23:54'),
+(4, 4, '2025-01-02T05:44:10');
